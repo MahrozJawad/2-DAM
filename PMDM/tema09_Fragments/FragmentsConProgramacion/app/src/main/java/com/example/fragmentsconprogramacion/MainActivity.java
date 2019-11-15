b8f7468b482f18ff59dcc6a699e3c498f13014f8
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2019-11-15 11:21:06 +0100

    "Gestionar fragments "

---
+++ PMDM/tema09_Fragments/FragmentsConProgramacion/app/src/main/java/com/example/fragmentsconprogramacion/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.fragmentsconprogramacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        Fragment fragment = new Fragment2();
+        ft.add(R.id.fragment_container, fragment);
+        ft.commit();
+    }
+}
