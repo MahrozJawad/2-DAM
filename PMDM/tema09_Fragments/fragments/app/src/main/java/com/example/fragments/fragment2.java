b5e33f41a4d18b8d3317fe0f883a6b2573b4c1c5
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2019-11-15 10:40:03 +0100

    "Fragments"

--- PMDM/tema09_Fragments/fragments/app/src/main/java/com/example/fragments/fragment2.java
+++ PMDM/tema09_Fragments/fragments/app/src/main/java/com/example/fragments/fragment2.java
@@ -9,10 +9,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class fragment1 extends Fragment {
+public class fragment2 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment1, container, false);
+        return inflater.inflate(R.layout.fragment2, container, false);
     }
 }
