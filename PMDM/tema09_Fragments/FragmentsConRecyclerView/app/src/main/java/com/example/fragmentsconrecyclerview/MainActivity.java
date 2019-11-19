639d8a3007be86229fbe6e6113993c2ed55a4459
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2019-11-19 11:24:46 +0100

    Android

---
+++ PMDM/tema09_Fragments/FragmentsConRecyclerView/app/src/main/java/com/example/fragmentsconrecyclerview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fragmentsconrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
