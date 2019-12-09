f5c28dbc8ebd1d9fa9fb70fd3c3f7099594a1162
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2019-12-09 15:03:44 +0100

    sfg

--- PSP/Tema4/Proyectos/PasarobjetosATravezDeSockets/src/pasarobjetosatravezdesockets/ClienteTCP.java
+++ PSP/Tema4/Proyectos/PasarobjetosATravezDeSockets/src/pasarobjetosatravezdesockets/ClienteTCP.java
@@ -1,9 +1,56 @@
-
 package pasarobjetosatravezdesockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ClienteTCP {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Socket miSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            miSocket = new Socket("localhost", 4444);
+            out = new PrintWriter(miSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(miSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: manis.csi.ull.es.");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: manis.csi.ull.es.");
+            System.exit(1);
+        }
+
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String fromServer;
+        String fromUser;
         
+        ObjectOutputStream outObject = new ObjectOutputStream(miSocket.getOutputStream());
+        ObjectInputStream inObject = new ObjectInputStream(miSocket.getInputStream());
+        
+        while ((fromServer = in.readLine()) != null) {
+            System.out.println("Server: " + fromServer);
+            if (fromServer.equals("Bye.")) {
+                break;
+            }
+
+            fromUser = stdIn.readLine();
+            if (fromUser != null) {
+                System.out.println("Client: " + fromUser);
+                out.println(fromUser);
+            }
+        }
+
+        out.close();
+        in.close();
+        stdIn.close();
+        miSocket.close();
     }
 }
--- PSP/Tema4/Proyectos/PasarobjetosATravezDeSockets/src/pasarobjetosatravezdesockets/ServidorTCP.java
+++ PSP/Tema4/Proyectos/PasarobjetosATravezDeSockets/src/pasarobjetosatravezdesockets/ServidorTCP.java
@@ -1,9 +1,56 @@
 
 package pasarobjetosatravezdesockets;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServidorTCP {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(4444);
+	    System.out.println("estoy después de crear el socket");
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+	    System.out.println("estoy después de aceptar un cliente");
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+				clientSocket.getInputStream()));
+        String inputLine, outputLine;
+        ObjectOutputStream outObject = new ObjectOutputStream(clientSocket.getOutputStream());
+        ObjectInputStream inObject = new ObjectInputStream(clientSocket.getInputStream());
+
+        outputLine = kkp.processInput(null);
+        out.println(outputLine);
+
+        while ((inputLine = in.readLine()) != null) {
+             outputLine = kkp.processInput(inputLine);
+             out.println(outputLine);
+             if (outputLine.equals("Bye."))
+                break;
+        }
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
     }
 }
