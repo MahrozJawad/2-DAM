07a017152f37a6f71b1f2530e0c8f3bfde67e8da
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2020-01-07 13:33:07 +0100

    Mi Commit cada dia

---
+++ DINT/Informes/Actividad_6.1_MahrozJawad/Informes/Informe1/Window1.xaml.vb
@@ -0,0 +1,35 @@
+﻿Class Window1
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        reportViewer1.Owner = Me
+        reportViewer2.Owner = Me
+        reportViewer3.Owner = Me
+        reportViewer4.Owner = Me
+        reportViewer5.Owner = Me
+
+        Dim informe1 As New CrystalReport1
+        Dim informe2 As New CrystalReport2
+        Dim informe3 As New CrystalReport3
+        Dim informe4 As New CrystalReport4
+        Dim informe5 As New CrystalReport5
+
+        informe1.SetDatabaseLogon("administrador", "admin123.")
+        informe2.SetDatabaseLogon("administrador", "admin123.")
+        informe3.SetDatabaseLogon("administrador", "admin123.")
+        informe4.SetDatabaseLogon("administrador", "admin123.")
+        informe5.SetDatabaseLogon("administrador", "admin123.")
+
+        reportViewer1.ViewerCore.ReportSource = informe1
+        reportViewer2.ViewerCore.ReportSource = informe2
+        reportViewer3.ViewerCore.ReportSource = informe3
+        reportViewer4.ViewerCore.ReportSource = informe4
+        reportViewer5.ViewerCore.ReportSource = informe5
+
+    End Sub
+End Class
